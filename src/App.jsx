f43991b308e982ab200b23adc61b80751c277143
jimmy <keekeechoi91@gmail.com>
2026-05-26 10:05:28 +0900

    feat: 라우팅 및 페이지 기본 틀 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Navbar from "./components/common/Navbar";
+import Footer from "./components/common/Footer";
 import BookListPage from "./pages/BookListPage";
 import BookDetailPage from "./pages/BookDetailPage";
 import BookCreatePage from "./pages/BookCreatePage";
